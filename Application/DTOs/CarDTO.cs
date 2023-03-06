9b909d929f34ca7150788b147101717291060407
sptmbr <imorexy@gmail.com>
2023-03-06 20:06:51 +0300

    feat: add xml response

--- Application/DTOs/CarDTO.cs
+++ Application/DTOs/CarDTO.cs
@@ -1,6 +1,10 @@
 using Domain.Entity;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
 
 namespace Application.DTOs;
+
+[DataContract(Name = "CarDTO", Namespace = "")]
 public class CarDTO
 {
     public CarDTO(Car car)
@@ -11,10 +15,14 @@ public class CarDTO
         this.C_Model = car.C_Model;
         this.C_Details = car.C_Details;
     }
+    [DataMember]
     public int Id { get; set; }
+    [DataMember]
     public int CarManufacturerId { get; set; }
+    [DataMember]
     public DateTime YearOfManufacture { get; set; } = DateTime.Now;
+    [DataMember]
     public string C_Model { get; set; } = null!;
+    [DataMember]
     public string? C_Details { get; set; } = null;
-    public CarManufacturer? CarManufacturer { get; set; }
 }
--- SuppliersWebApi/Program.cs
+++ SuppliersWebApi/Program.cs
@@ -12,7 +12,7 @@ namespace SuppliersWebApi
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddXmlDataContractSerializerFormatters();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
